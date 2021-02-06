b839c55622b6d7f12971486647172e619e5ae57d
alexandre.lima@solluti.com.br <alexandre.lima@solluti.com.br>
2021-02-06 18:32:39 -0300

    debugging android

--- js/section5Form.js
+++ js/section5Form.js
@@ -20,8 +20,28 @@ function formSubmit(event) {
   event.target.reset();
 }
 
+function stringify_object(object, depth = 0, max_depth = 2) {
+  // change max_depth to see more levels, for a touch event, 2 is good
+  if (depth > max_depth) return "Object";
+
+  const obj = {};
+  for (let key in object) {
+    let value = object[key];
+    if (value instanceof Node)
+      // specify which properties you want to see from the node
+      value = { id: value.id };
+    else if (value instanceof Window) value = "Window";
+    else if (value instanceof Object)
+      value = stringify_object(value, depth + 1, max_depth);
+
+    obj[key] = value;
+  }
+
+  return depth ? obj : JSON.stringify(obj);
+}
+
 function mask(event, input, maskType) {
-  alert(JSON.stringify(event));
+  alert(stringify_object(event));
   if (
     event.key.length === 1 &&
     event.altKey == false &&
